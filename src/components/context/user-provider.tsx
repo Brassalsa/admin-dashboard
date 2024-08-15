711f657c4e903b9887dd7952a393feae719ecb7f
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-15 13:59:49 +0530

    add theme, user ctx

---
+++ src/components/context/user-provider.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { generateUser } from "@/lib/utils/user";
+import { User } from "@/types";
+import {
+  createContext,
+  Dispatch,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const UserCtx = createContext<{
+  user: User | null;
+  setUser: Dispatch<User | null>;
+} | null>(null);
+
+export const useUserCtx = () => {
+  const ctx = useContext(UserCtx);
+  if (!ctx) {
+    throw new Error("useUserCtx must be used inside UserProvider");
+  }
+  return ctx;
+};
+
+export default function UserProvider({ children }: PropsWithChildren) {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    if (!user) setUser(generateUser());
+  }, []);
+
+  return <UserCtx.Provider value={{ user, setUser }} children={children} />;
+}
