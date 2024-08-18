71b653d9454a7c0629c5cfdaa24abddc1a0a13b3
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-18 22:27:20 +0530

    style changes

--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -1,13 +1,18 @@
 "use client";
 import React from "react";
 import { useUserCtx } from "./context/user-provider";
+import { Card, CardContent } from "./ui/card";
 
 function Overview() {
   const { user } = useUserCtx();
 
   return (
-    <div className="h-svh">
-      <h1 className="text-xl">Welcome, {user?.name}</h1>
+    <div>
+      <Card className="min-w-[80svw] h-[80svh]">
+        <CardContent className="py-2">
+          <h1 className="text-2xl font-semibold">Welcome, {user?.name}</h1>
+        </CardContent>
+      </Card>
     </div>
   );
 }
