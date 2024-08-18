31a48c376a732e3fd5112ae610c1aa3d540a17e2
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-18 19:57:07 +0530

    add menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,25 @@ import FloatingContainer from "./ui/floating-container";
 import NavBar from "./ui/navbar";
 import Logo from "./logo";
 import { ModeToggle } from "./theme-toogle";
-import UserUI, { UserUIImg, UserUiName } from "./user-ui";
+import UserUI, { UserUIImg, UserUiName, UserUIUserName } from "./user-ui";
 import { Button } from "./ui/button";
-import { Bell } from "lucide-react";
+import { Bell, MenuIcon } from "lucide-react";
 import { useUserCtx } from "./context/user-provider";
-import Menu from "./menu";
+import Menu, { MenuSideBarLink } from "./menu";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTrigger,
+} from "./ui/sheet";
+import { useState } from "react";
 
 function Header() {
   const { user } = useUserCtx();
+  const [openMenu, setOpenMenu] = useState(false);
+  const trigger = () => setOpenMenu(!openMenu);
   return (
     <FloatingContainer className="min-h-14 border-b-2 bg-background/90 z-50 mb-4 flex justify-center items-center">
       <NavBar className="h-full w-full">
@@ -22,13 +33,44 @@ function Header() {
         >
           <Bell className="stroke-slate-600 dark:stroke-slate-200 size-4" />
         </Button>
-        <Menu />
+        <div className="sm:flex gap-2 hidden ">
+          <Menu />
+        </div>
         {user && (
-          <UserUI {...user} className="flex gap-2">
+          <UserUI {...user} className="sm:flex gap-2 hidden">
             <UserUIImg />
             <UserUiName className="text-xs" />
           </UserUI>
         )}
+        <Sheet open={openMenu} onOpenChange={setOpenMenu}>
+          <SheetTrigger className="sm:hidden">
+            <MenuIcon className="size-4 stroke-slate-500 dark:stroke-slate-300" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>Menu</SheetHeader>
+            <SheetDescription className="flex flex-col gap-2 mt-6">
+              <MenuSideBarLink onClick={trigger} href={"/"}>
+                Overview
+              </MenuSideBarLink>
+              <MenuSideBarLink onClick={trigger} href={"/people"}>
+                People
+              </MenuSideBarLink>
+
+              {user && (
+                <UserUI
+                  {...user}
+                  className="flex gap-2 mt-auto absolute bottom-4"
+                >
+                  <UserUIImg className="size-12" />
+                  <div>
+                    <UserUiName className="text-sm text-primary" />
+                    <UserUIUserName />
+                  </div>
+                </UserUI>
+              )}
+            </SheetDescription>
+          </SheetContent>
+        </Sheet>
       </NavBar>
     </FloatingContainer>
   );
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,9 @@
 import { PropsDefault } from "@/types";
-import React from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 import FancyLink from "./ui/fancy-link";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Separator } from "./ui/separator";
 
 export default function Menu() {
   return (
@@ -13,4 +16,21 @@ export default function Menu() {
 
 export function MenuItemLink(props: PropsDefault & { href: string }) {
   return <FancyLink {...props} />;
+}
+
+export function MenuSideBarLink(
+  props: PropsDefault & { href: string } & ComponentPropsWithoutRef<"a">
+) {
+  return (
+    <>
+      <Link
+        {...props}
+        className={cn(
+          "hover:bg-muted w-full p-1 text-base hover:text-primary rounded-md px-2",
+          props.className
+        )}
+      />
+      <Separator className="-mt-1" />
+    </>
+  );
 }
