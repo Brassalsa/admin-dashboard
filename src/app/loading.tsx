be3a83aae28c9774607a28ed82dd2778c387f60a
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-18 20:52:07 +0530

    add loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Loading() {
-  return <div>Loading...</div>;
+  const className = "h-2.5 w-2.5 rounded-full bg-muted-foreground";
+  return (
+    <div className="flex space-x-1 w-40 absolute top-[45%] left-[45%]">
+      <div className={`${className} animate-bounce`}></div>
+      <div className={`${className} animate-bounce200`}></div>
+      <div className={`${className} animate-bounce400`}></div>
+    </div>
+  );
 }
 
 export default Loading;
