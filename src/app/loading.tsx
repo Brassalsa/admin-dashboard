db21615b8d90f42dbe31edcb0a8c7b505a09f345
Brassalsa <sauravchauhan2k@gmail.com>
2024-08-25 12:37:36 +0530

    fix loading ui

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,8 +5,8 @@ function Loading() {
   return (
     <div className="flex space-x-1 w-40 absolute top-[45%] left-[45%]">
       <div className={`${className} animate-bounce`}></div>
-      <div className={`${className} animate-bounce200`}></div>
-      <div className={`${className} animate-bounce400`}></div>
+      <div className={`${className} animate-bounce delay-200`}></div>
+      <div className={`${className} animate-bounce delay-400`}></div>
     </div>
   );
 }
